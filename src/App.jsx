7f05fd1f7196b60f656d503330c1e8d08e632988
Minja Ćućula <cuculaminja@proton.me>
2025-01-20 20:44:15 +0100

    Add header button

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ function App() {
           <span className="heading-primary-main">Outdoors</span>
           <span className="heading-primary-sub">is where life happens</span>
         </h1>
+
+        <a href="#" className="btn btn-white btn-animated">
+          Discover our tours
+        </a>
       </div>
     </header>
   );
