01779e0e698b966d9a9a9fef37ef58058dc8c471
Minja Ćućula <cuculaminja@proton.me>
2025-01-28 15:16:06 +0100

    Converting to Sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import "./App.css";
+import "./sass/main.scss";
 
 import logo from "./imgs/logo-white.png";
 
 function App() {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={logo} alt="Logo of Natours" className="logo" />
+      <div className="header__logo-box">
+        <img src={logo} alt="Logo of Natours" className="header__logo" />
       </div>
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
 
-        <a href="#" className="btn btn-white btn-animated">
+        <a href="#" className="btn btn--white btn--animated">
           Discover our tours
         </a>
       </div>
