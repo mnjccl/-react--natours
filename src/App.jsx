9b7e2f5fe10aeeae9bfe02b5ac0c0c56c15903b2
Minja Ćućula <cuculaminja@proton.me>
2025-02-27 23:04:20 +0100

    Add float template

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
+import Header from "./components/Header";
 import "./sass/main.scss";
 
-import logo from "./imgs/logo-white.png";
-
 function App() {
-  return (
-    <header className="header">
-      <div className="header__logo-box">
-        <img src={logo} alt="Logo of Natours" className="header__logo" />
-      </div>
-      <div className="header__text-box">
-        <h1 className="heading-primary">
-          <span className="heading-primary--main">Outdoors</span>
-          <span className="heading-primary--sub">is where life happens</span>
-        </h1>
-
-        <a href="#" className="btn btn--white btn--animated">
-          Discover our tours
-        </a>
-      </div>
-    </header>
-  );
+  return <Header />;
 }
 
 export default App;
