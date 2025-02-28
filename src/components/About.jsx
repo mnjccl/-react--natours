31b780df735a3dca71b89606c11fa67d8db40554
Minja Ćućula <cuculaminja@proton.me>
2025-02-28 17:29:46 +0100

    Add images to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,7 @@
+import nat1 from "../imgs/nat-1-large.jpg";
+import nat2 from "../imgs/nat-2-large.jpg";
+import nat3 from "../imgs/nat-3-large.jpg";
+
 function About() {
   return (
     <section className="section-about">
@@ -32,7 +36,29 @@ function About() {
           </a>
         </div>
 
-        <div className="col-1-of-2">Image</div>
+        <div className="col-1-of-2">
+          <div className="composition">
+            <img
+              src={nat1}
+              alt="Photo 1"
+              className="composition__photo composition__photo--p1"
+            />
+          </div>
+          <div className="composition">
+            <img
+              src={nat2}
+              alt="Photo 2"
+              className="composition__photo composition__photo--p2"
+            />
+          </div>
+          <div className="composition">
+            <img
+              src={nat3}
+              alt="Photo 3"
+              className="composition__photo composition__photo--p3"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
