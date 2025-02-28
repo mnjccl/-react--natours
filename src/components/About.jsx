9be8598b814cccb5e15b5c7b6837b8a5d5f9d2fe
Minja Ćućula <cuculaminja@proton.me>
2025-02-28 16:24:06 +0100

    Add text to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,37 @@
 function About() {
   return (
     <section className="section-about">
-      <div className="u-center-text u-margin-bottom-8">
+      <div className="u-center-text u-margin-bottom-big">
         <h2 className="heading-secondary">
           Exciting tours for adventurous people
         </h2>
       </div>
+
       <div className="row">
-        <div className="col-1-of-2">Text</div>
+        <div className="col-1-of-2">
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            You&apos;re going to fall in love with nature
+          </h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+            voluptates? Incidunt soluta ex harum. Repudiandae nostrum maxime
+            exercitationem deserunt temporibus sed eveniet cumque possimus
+            mollitia!
+          </p>
+
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            Live adventures like you never have before
+          </h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+            voluptates? Incidunt soluta ex harum.
+          </p>
+
+          <a href="#" className="btn-text">
+            Learn more &rarr;
+          </a>
+        </div>
+
         <div className="col-1-of-2">Image</div>
       </div>
     </section>
