66d15156cd72c407fb8a91c103ce52425e256684
Minja Ćućula <cuculaminja@proton.me>
2025-03-03 09:26:24 +0100

    Styling back side

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -6,6 +6,7 @@ function Tours() {
       </div>
 
       <div className="row">
+        {/* FIRST CARD */}
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">
@@ -25,26 +26,87 @@ function Tours() {
                 </ul>
               </div>
             </div>
+
             <div className="card__side card__side--back card__side--back-1">
-              Back
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">$297</p>
+                </div>
+                <a href="#" className="btn btn--white">
+                  Book now!
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
+        {/* SECOND CARD */}
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">Front</div>
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture-2"></div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span-2">
+                  The Forest Hiker
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>7 day tour</li>
+                  <li>Up to 40 people</li>
+                  <li>6 tour guides</li>
+                  <li>Sleep in provided tents</li>
+                  <li>Difficulty: medium</li>
+                </ul>
+              </div>
+            </div>
+
             <div className="card__side card__side--back card__side--back-2">
-              Back
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">$497</p>
+                </div>
+                <a href="#" className="btn btn--white">
+                  Book now!
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
+        {/* THIRD CARD */}
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">Front</div>
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture-3"></div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span-3">
+                  The Snow Adventurer
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>5 day tour</li>
+                  <li>Up to 15 people</li>
+                  <li>3 tour guides</li>
+                  <li>Sleep in provided tents</li>
+                  <li>Difficulty: hard</li>
+                </ul>
+              </div>
+            </div>
+
             <div className="card__side card__side--back card__side--back-3">
-              Back
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">$897</p>
+                </div>
+                <a href="#" className="btn btn--white">
+                  Book now!
+                </a>
+              </div>
             </div>
           </div>
         </div>
