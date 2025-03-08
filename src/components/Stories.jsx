000f645f1208d13113c7418e085a32bb42fee511
Minja Ćućula <cuculaminja@proton.me>
2025-03-08 19:44:20 +0100

    Fix source typo

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -9,7 +9,7 @@ function Stories() {
       <div className="bg-video">
         <video className="bg-video__content" autoPlay muted loop>
           <source src={video} type="video/mp4" />
-          <sorce src={video2} type="video/webm" />
+          <source src={video2} type="video/webm" />
           Your browser is not supported!
         </video>
       </div>
