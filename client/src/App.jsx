135a936d8a97261ec25e6f323ebe5c71c1a8ae5f
Minja Ćućula <97902635+mnjccl@users.noreply.github.com>
2026-01-22 16:41:17 +0100

    Add login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import About from "./components/About";
+/*import About from "./components/About";
 import Booking from "./components/Booking";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
@@ -6,22 +6,16 @@ import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Popup from "./components/Popup";
 import Stories from "./components/Stories";
-import Tours from "./components/Tours";
+import Tours from "./components/Tours";*/
+
+import Login from "./components/Login";
 
 import "./sass/main.scss";
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Header />
-      <About />
-      <Features />
-      <Tours />
-      <Stories />
-      <Booking />
-      <Footer />
-      <Popup />
+      <Login />
     </>
   );
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,46 @@
+function Login() {
+  return (
+    <main>
+      <div className="login-content">
+        <h2 className="heading-secondary ma-bt-lg">Log into your account</h2>
+
+        <form action="#" className="form">
+          <div className="form__group">
+            <label className="form__label" htmlFor="email">
+              Email address
+            </label>
+            <input
+              id="email"
+              className="form__input"
+              type="email"
+              placeholder="you@example.com"
+              required
+            />
+          </div>
+
+          <div className="form__group ma-bt-md">
+            <label className="form__label" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              className="form__input"
+              type="password"
+              placeholder="••••••••"
+              required
+              minLength={8}
+            />
+          </div>
+
+          <div className="form__group">
+            <button className="btn btn--green" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default Login;
